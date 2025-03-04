676ec4078448ef7dfe660d1a4dd1faabe03d42d4
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-03 19:07:50 -0800

    feat: implement async data fetching for Action and Arnold pages

--- src/movapages.rs
+++ src/movapages.rs
@@ -1,15 +1,72 @@
-use leptos::prelude::*;
+#![allow(non_snake_case)]
+use leptos::{prelude::*, task::spawn_local};
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+struct Infos {
+    Name: String,
+    HttpThumbPath: String,
+}
 
 #[component]
 pub fn ActionPage() -> impl IntoView {
+    let (infos, set_infos) = signal(Vec::new());
+
+    spawn_local(async move {
+        match fetch_action().await {
+            Ok(data) => {
+                log::info!("Fetched infos data: {:?}", data); // Debugging log
+                set_infos.set(data);
+            },
+            Err(err) => log::error!("Error fetching infos data: {:?}", err),
+        }
+    });
+
     view! {
-        <h1>"Action"</h1>
+        <div class="mov-grid">
+            {move || infos.get().iter().map(|info| view! {
+                <div class="mov-item">
+                    <img src={info.HttpThumbPath.clone()} alt={info.Name.clone()} />
+                    <p>{info.Name.clone()}</p>
+                </div>
+            }).collect_view()}
+        </div>
     }
+}
+async fn fetch_action() -> Result<Vec<Infos>, Error> {
+    let response = reqwest::get("http://10.0.4.41:7777/action").await?;
+    let action: Vec<Infos> = response.json().await?;
+    Ok(action)
 }
 
 #[component]
 pub fn ArnoldPage() -> impl IntoView {
+    let (infos, set_infos) = signal(Vec::new());
+
+    spawn_local(async move {
+        match fetch_arnold().await {
+            Ok(data) => {
+                log::info!("Fetched infos data: {:?}", data); // Debugging log
+                set_infos.set(data);
+            },
+            Err(err) => log::error!("Error fetching infos data: {:?}", err),
+        }
+    });
+
     view! {
-        <h1>"Arnold"</h1>
+        <div class="mov-grid">
+            {move || infos.get().iter().map(|info| view! {
+                <div class="mov-item">
+                    <img src={info.HttpThumbPath.clone()} alt={info.Name.clone()} />
+                    <p>{info.Name.clone()}</p>
+                </div>
+            }).collect_view()}
+        </div>
     }
+}
+async fn fetch_arnold() -> Result<Vec<Infos>, Error> {
+    let response = reqwest::get("http://10.0.4.41:7777/arnold").await?;
+    let arnold: Vec<Infos> = response.json().await?;
+    Ok(arnold)
 }
