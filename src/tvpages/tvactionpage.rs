c490c849d8720368452e573180d81290167bfd01
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-05 03:18:32 -0800

    feat: add TV pages for Action, Comedy, Fantasy, MCU, Science, Sci-Fi, Star Trek, Star Wars, and Western with updated styles

---
+++ src/tvpages/tvactionpage.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+use leptos::{prelude::*, task::spawn_local};
+use reqwest::Error;
+use serde::Deserialize;
+
+
+#[component]
+pub fn TVActionPage() -> impl IntoView {
+    view! {
+        <div class="tv-grid">
+                <div class="tv-item">
+                    <img src="http://10.0.4.41:9090/shogun.webp" alt="Showgun" />
+                    <ing src="http://10.0.4.41:9090/continental.webp" alt="Continental" />
+                    
+                </div>
+            
+        </div>
+    }
+}
