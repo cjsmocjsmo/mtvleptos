c351fe452d53b2ddc04fdf72b7a869963f757f16
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-05 11:24:18 -0800

    style: update images in TVStarWarsPage for improved content relevance

--- src/tvpages/tvstarwarspage.rs
+++ src/tvpages/tvstarwarspage.rs
@@ -8,12 +8,12 @@ pub fn TVStarWarsPage() -> impl IntoView {
                 <img src="http://10.0.4.41:9090/skeletoncrew.webp" alt="Secret Invation" />
                 <img src="http://10.0.4.41:9090/ahsoka.webp" alt="I Am Groot" />
                 <img src="http://10.0.4.41:9090/andor.webp" alt="Loki" />
-                <img src="http://10.0.4.41:9090/nextgen.webp" alt="MoonKnight" />
-                <img src="http://10.0.4.41:9090/discovery.webp" alt="SheHulk" />
-                <img src="http://10.0.4.41:9090/picard.webp" alt="Hawkeye" />
-                <img src="http://10.0.4.41:9090/lowerdecks.webp" alt="Falcon And The Winter Soldier" />
-                <img src="http://10.0.4.41:9090/prodigy.webp" alt="Foundation" />
-                <img src="http://10.0.4.41:9090/strangenewworlds.webp" alt="Altered Carbon" />
+                <img src="http://10.0.4.41:9090/badbatch.webp" alt="MoonKnight" />
+                <img src="http://10.0.4.41:9090/bobafett.webp" alt="SheHulk" />
+                <img src="http://10.0.4.41:9090/mandalorian.webp" alt="Hawkeye" />
+                <img src="http://10.0.4.41:9090/talesofthejedi.webp" alt="Falcon And The Winter Soldier" />
+                <img src="http://10.0.4.41:9090/obiwan.webp" alt="Foundation" />
+                <img src="http://10.0.4.41:9090/visions.webp" alt="Altered Carbon" />
         </div>
     }
 }
