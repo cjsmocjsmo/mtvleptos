58e7cb8b890e43db0c6da991af02d388ea0d7a58
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-07 11:47:03 -0800

    style: add search input fields to SearchPage for improved user interaction

--- src/searchpage.rs
+++ src/searchpage.rs
@@ -5,5 +5,7 @@ pub fn SearchPage() -> impl IntoView {
 
     view! {
         <h1>"SearchPage"</h1>
+        <input type="text" placeholder="Search..." />
+        <input type="submit" value="Search" />
     }
 }
