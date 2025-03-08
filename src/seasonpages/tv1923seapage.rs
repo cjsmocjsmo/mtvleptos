b088a33b4021d8b6d8f64bbf64156ead9bd2c67f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-08 10:06:56 -0800

    style: refactor layout and styling for TV1923SeaPage, adjusting button group and header placement

--- src/seasonpages/tv1923seapage.rs
+++ src/seasonpages/tv1923seapage.rs
@@ -6,11 +6,13 @@ pub fn TV1923SeaPage() -> impl IntoView {
     view! {
         <div class="seaMainDiv">
             <h1 class="seaH1">1923</h1>
-            <h3 class="seaH3">Season 1</h3>
             <div class="seaInnerDiv">
-                <button class="seaBtn">1</button>
-                <button class="seaBtn">2</button>
-                <button class="seaBtn">3</button>
+                <h3 class="seaH3">Season 1</h3>
+                <div class="seaBtnGrp">
+                    <button class="seaBtn">1</button>
+                    <button class="seaBtn">2</button>
+                    <button class="seaBtn">3</button>
+                </div>
             </div>
         </div>
     }
