7f83094c2774bc7deff9a230e5f69809ad1e6f29
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-03-11 21:45:22 -0700

    style: fix closure to retrieve movie infos in ActionPage function

--- src/movpages/movapages.rs
+++ src/movpages/movapages.rs
@@ -42,7 +42,7 @@ pub fn ActionPage() -> impl IntoView {
 
     view! {
         <div class="mov-row">
-            {let infos = infos.get().clone(); move || infos.iter().map(|info| {
+            {let infos = move || infos.get().clone(); move || infos().iter().map(|info| {
                 // let info = info.clone();
                 view! {
                     <img 
