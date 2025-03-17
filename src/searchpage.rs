d8d9e1a0ac15a0fb691d3d0e78fb0ce4e43a82fd
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-16 21:33:10 -0700

    style: add empty spans to searchDiv for improved layout flexibility

--- src/searchpage.rs
+++ src/searchpage.rs
@@ -2,13 +2,14 @@ use leptos::prelude::*;
 
 #[component]
 pub fn SearchPage() -> impl IntoView {
-
     view! {
         <div class="searchDiv">
             <div class="searchInnerDiv">
                 <input class="search-input" type="text" placeholder="Search..." />
                 <button class="searchButton">Search</button>
             </div>
+            <span></span>
+            <span></span>
         </div>
     }
 }
