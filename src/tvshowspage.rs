f812564214bf0b460baeb19af891348a2b9a4733
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-16 20:10:05 -0700

    style: replace div with main for semantic structure and add empty span for markup consistency

--- src/tvshowspage.rs
+++ src/tvshowspage.rs
@@ -3,19 +3,21 @@ use leptos::prelude::*;
 #[component]
 pub fn TVShowsListPage() -> impl IntoView {
     view! {
-        <div class="tvshows-section">
-            <div class="tvshows-sectionDiv">
-                <a class="tvshows-sectionDivItem" href="/tvactionpage">"Action"</a>
-                <a class="tvshows-sectionDivItem" href="/tvcomedypage">"Comedy"</a>
-                <a class="tvshows-sectionDivItem" href="/tvfantasypage">"Fantasy"</a>
-                <a class="tvshows-sectionDivItem" href="/tvmcupage">"MCU"</a>
-                <a class="tvshows-sectionDivItem" href="/tvscience">"Science"</a>
-                <a class="tvshows-sectionDivItem" href="/tvscifi">"SciFi"</a>
-                <a class="tvshows-sectionDivItem" href="/tvstartrek">"Star Trek"</a>
-                <a class="tvshows-sectionDivItem" href="/tvstarwars">"Star Wars"</a>
-                <a class="tvshows-sectionDivItem" href="/tvwesterns">"Westerns"</a>
+        <main>
+            <div class="tvshows-section">
+                <div class="tvshows-sectionDiv">
+                    <a class="tvshows-sectionDivItem" href="/tvactionpage">"Action"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvcomedypage">"Comedy"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvfantasypage">"Fantasy"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvmcupage">"MCU"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvscience">"Science"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvscifi">"SciFi"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvstartrek">"Star Trek"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvstarwars">"Star Wars"</a>
+                    <a class="tvshows-sectionDivItem" href="/tvwesterns">"Westerns"</a>
+                </div>
+                <span></span>
             </div>
-        </div>
-        <span></span>
+        </main>
     }
 }
