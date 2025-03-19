65c2bcdafdcff09c4f8972b891a4c05550887da5
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-19 04:51:38 -0700

    style: refactor search form to use Leptos Form component for improved functionality

--- src/searchpage.rs
+++ src/searchpage.rs
@@ -1,12 +1,28 @@
 use leptos::prelude::*;
+use leptos::use_query_map;
+use leptos::html::Form;
+
+async fn fetch_results() {
+
+}
 
 #[component]
 pub fn SearchPage() -> impl IntoView {
+    // reactive access to URL query strings
+    let query = use_query_map();
+    // search stored as ?q=
+    let search = move || query.read().get("q").unwrap_or_default();
+    // a resource driven by the search string
+    let search_results = Resource::new(search, |_| fetch_results());
+
     view! {
         <div class="searchDiv">
             <div class="searchInnerDiv">
-                <input class="search-input" type="text" placeholder="Search..." />
-                <button class="searchButton">Search</button>
+		        <Form method="GET" action="">
+                	<input class="search-input" type="text" placeholder="Search..." />
+                	// <button class="searchButton">Search</button>
+			        <input class="searchButton" type="submit" />
+		        </Form>
             </div>
             <span></span>
             <span></span>
